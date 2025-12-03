d8c6847f52ebfe271a1b8d74b7d43c47dac59028
Nunes-n <realhelicoma@gmail.com>
2025-12-03 11:06:53 -0300

    Update Exercicio3.c

--- Estruturas de Dados/Lista 2/Exercicio3.c
+++ Estruturas de Dados/Lista 2/Exercicio3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int emax(int[], int, int);
-int emin(int[], int, int);
+int emax(int[], int);
+int emin(int[], int);
 
 int main (void)
 {
@@ -10,6 +10,11 @@ int main (void)
 
     while (1)
     {
+        for (int i = 0; i < 26; i++)
+        {
+            lista[i] = 9999;
+        }
+
         printf ("quantidade de nos: ");
         if (scanf("%d", &qtde) == EOF)
             return 0;
@@ -22,7 +27,7 @@ int main (void)
         {
             for (int i = 1; i <= qtde; i++)
             {
-                printf ("valor do no: ");
+                printf ("\nvalor do no: ");
                 scanf ("%d", &valor);
                 if (valor < -1000 || valor > 1000)
                 {
@@ -35,12 +40,12 @@ int main (void)
                 }
             }
 
-            if (emax(lista, 1, qtde + 1) == 0)
+            if (emax(lista, 1) == 0)
             {
                 printf ("max\n");
             }
             else
-                if (emin(lista, 1, qtde + 1) == 0)
+                if (emin(lista, 1) == 0)
                 {
                     printf ("min\n");
                 }
@@ -55,19 +60,58 @@ int main (void)
     return 0;
 }
 
-int emax (int lista[], int n, int qtd)
+int emax (int lista[], int n)
 {
     int filho1, filho2;
 
+    if (lista[n * 2] != 9999 && lista[n * 2 + 1] != 9999)
+    {
+        if (lista[n] > lista[n * 2] && lista[n] > lista[n * 2 + 1])
+        {
+            filho1 = emax(lista, n * 2);
+            filho2 = emax(lista, n * 2 + 1);
+
+            if (filho1 == 0 && filho2 == 0)
+                return 0;
+            else return 1;
+        }
+    }
+    else
+        if (lista[n * 2] != 9999)
+        {
+            if (lista[n] > lista[n * 2])
+            {
+                filho1 = emax(lista, n * 2);
+
+                if (filho1 == 0)
+                    return 0;
+                else return 1;
+            }
+        }
+        else
+            if (lista[n * 2 + 1] != 9999)
+            {
+                if (lista[n] > lista[n * 2 + 1])
+                {
+                    filho2 = emax(lista, n * 2 + 1);
+
+                    if (filho2 == 0)
+                        return 0;
+                    else return 1;
+                }
+            }
+            else return 0;
+
+/*
     if (lista[n] > lista[n * 2] && lista[n] > lista[n * 2 + 1])
     {
-        if ((n * 2) < qtd) //filho da esquerda
-            filho1 = emax(lista, n * 2, qtd);
+        if (lista[n * 2] != 9999) //filho da esquerda
+            filho1 = emax(lista, n * 2);
         else
             filho1 = 0;
 
-        if ((n * 2 + 1) < qtd) //filho da direita
-            filho2 = emax(lista, n * 2 + 1, qtd);
+        if (lista[n * 2 + 1] != 9999) //filho da direita
+            filho2 = emax(lista, n * 2 + 1);
         else
             filho2 = 0;
 
@@ -78,21 +122,22 @@ int emax (int lista[], int n, int qtd)
             return 1;
     }
     else return 1;
+    */
 }
 
-int emin (int lista[], int n, int qtd)
+int emin (int lista[], int n)
 {
     int filho1, filho2;
 
     if (lista[n] < lista[n * 2] && lista[n] < lista[n * 2 + 1])
     {
-        if ((n * 2) < qtd) //filho da esquerda
-            filho1 = emin(lista, n * 2, qtd);
+        if (lista[n * 2] != 9999) //filho da esquerda
+            filho1 = emin(lista, n * 2);
         else
             filho1 = 0;
 
-        if ((n * 2 + 1) < qtd) //dilho da direita
-            filho2 = emin(lista, n * 2 + 1, qtd);
+        if (lista[n * 2 + 1] != 9999) //dilho da direita
+            filho2 = emin(lista, n * 2 + 1);
         else
             filho2 = 0;
 
