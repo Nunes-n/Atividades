8dc7c78c615604058cc32c3c1ae66748985cf564
Nunes-n <realhelicoma@gmail.com>
2025-12-07 18:54:29 -0300

    Add files via upload

---
+++ Estruturas de Dados/Caderno de exercicios/Exercicio18.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TNo
+{
+    struct TNo *esq;
+    int valor;
+    struct TNo* dir;
+};
+typedef struct TNo TNo;
+
+int insereValor (TNo **, int);
+TNo* achaPai(TNo *, int);
+int Bytes(TNo *);
+
+int main(void)
+{
+    int valor;
+    TNo *raiz = NULL;
+
+    printf ("[-9999 para encerrar]\n");
+
+    while (1)
+    {
+        printf ("valor do no: ");
+        scanf ("%d", &valor);
+
+        if (valor != -9999)
+        {
+            if (insereValor(&raiz, valor) == 1)
+                printf ("erro: memoria insuficiente\n");
+        }
+        else break;
+    }
+
+    printf ("Total de bytes que a arvore ocupa: %d bytes \n", Bytes(raiz));
+
+    return 0;
+}
+
+int insereValor (TNo **r, int n)
+{
+    TNo *aux = (TNo *) malloc(sizeof(TNo));
+    if (aux == NULL)
+        return 1;
+
+    aux->valor = n;
+    aux->esq = NULL;
+    aux->dir = NULL;
+
+    TNo *pai = achaPai(*r, n);
+    if (pai == NULL)
+        *r = aux;
+    else
+        if (aux->valor > pai->valor)
+            pai->dir = aux;
+        else
+            pai->esq = aux;
+
+    return 0;
+}
+
+TNo* achaPai(TNo *r, int n)
+{
+    if (r == NULL) //arvore vazia, é a raiz
+        return NULL;
+
+    if (r->valor < n) // é o filho da direita
+    {
+        if (r->dir == NULL)
+            return r;
+        else
+            return achaPai(r->dir, n);
+    }
+    else //é o filho da esquerda
+    {
+        if (r->esq == NULL)
+            return r;
+        else
+            return achaPai(r->esq, n);
+    }
+}
+
+int Bytes(TNo *r)
+{
+    if (r == NULL)
+        return 0;
+    else
+        return (sizeof(TNo)) + Bytes(r->esq) + Bytes(r->dir);
+}
